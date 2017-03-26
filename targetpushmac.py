773455ed34ad91d5fbab593af3357e2b94c17cbf
lancelot <lancelot.b@outlook.com>
2017-03-26 20:04:16 +0800

    260317#01: add more instructions

--- targetpushmac.py
+++ targetpushmac.py
@@ -9,14 +9,14 @@ import re
 import sys
 from rpython.rlib import jit
 
-pmac_bytecode = [ "ldi", "pop", "out" ]
+pmac_bytecode = [ "ldi", "pop", "out", "add", "jmp", "jt", "lt", "eq", "not", "set", "get" ]
 pmac_bytecode = { name: i for i, name in enumerate(pmac_bytecode) }
 for k, v in pmac_bytecode.items():
 	globals()["pmac_bytecode_" + k] = v
 
 def err(msg):
 	print("error: " + msg)
-	sys.abort()
+	raise Exception(msg)
 
 def splitx(src, char = [ " ", "\t", "\n", "\r" ]):
 	ret = []
@@ -30,6 +30,22 @@ def splitx(src, char = [ " ", "\t", "\n", "\r" ]):
 	ret.append(src[beg:].strip())
 
 	return [ a for a in ret if len(a) ]
+
+def isint(src):
+	try:
+		int(src)
+		return 1
+	except:
+		return 0
+
+str_pool = []
+
+def reg_str(s):
+	str_pool.append(s)
+	return len(str_pool) - 1
+
+def get_str(i):
+	return str_pool[i]
 
 def parse(src):
 	# instr = { "ldi": , "pop", "out" }
@@ -56,17 +72,22 @@ def parse(src):
 
 	ret = splitx(src, [ "\r", "\n" ])
 	ret = [ splitx(l) for l in ret ]
-	ret = [ [ pmac_bytecode[l[0].lower()] ] + ([ int(arg) for arg in l[1:] ] if len(l) > 1 else []) for l in ret ]
+	ret = [ [ pmac_bytecode[l[0].lower()] ] + ([ int(arg) if isint(arg) else reg_str(arg) for arg in l[1:] ] if len(l) > 1 else []) for l in ret ]
 
 	return ret
 
 driver = jit.JitDriver(greens = [ "pc", "sp", "bc", "ins" ], reds = "auto")
+
+def _rt_check_stack(sp, req, msg = "insufficient stack"):
+	if sp < req:
+		err(msg)
 
 def run(bc):
 	pc = 0
 	stack = [ 0 ] * 1024
 	sp = 0
 	ins = None
+	glob = {}
 
 	while pc < len(bc):
 		driver.jit_merge_point(pc = pc, sp = sp, bc = bc, ins = ins)
@@ -78,12 +99,52 @@ def run(bc):
 			sp += 1
 
 		elif ins[0] == pmac_bytecode_pop:
-			if sp == 0: err("pop empty stack")
+			_rt_check_stack(sp, 1)
 			sp -= 1
 
 		elif ins[0] == pmac_bytecode_out:
-			if sp == 0: err("out empty stack")
+			_rt_check_stack(sp, 1)
 			print(stack[sp - 1])
+
+		elif ins[0] == pmac_bytecode_add:
+			_rt_check_stack(sp, 2)
+			stack[sp - 2] = stack[sp - 2] + stack[sp - 1]
+			sp -= 1
+
+		elif ins[0] == pmac_bytecode_jmp:
+			pc += ins[1]
+			continue
+
+		# jump if true
+		elif ins[0] == pmac_bytecode_jt:
+			_rt_check_stack(sp, 1)
+			sp -= 1
+			if stack[sp]:
+				pc += ins[1]
+				continue
+
+		elif ins[0] == pmac_bytecode_lt:
+			_rt_check_stack(sp, 2)
+			stack[sp - 2] = stack[sp - 2] < stack[sp - 1]
+			sp -= 1
+
+		elif ins[0] == pmac_bytecode_eq:
+			_rt_check_stack(sp, 2)
+			stack[sp - 2] = stack[sp - 2] == stack[sp - 1]
+			sp -= 1
+
+		elif ins[0] == pmac_bytecode_not:
+			_rt_check_stack(sp, 1)
+			stack[sp - 1] = not stack[sp - 1]
+
+		elif ins[0] == pmac_bytecode_set:
+			_rt_check_stack(sp, 1)
+			glob[get_str(ins[1])] = stack[sp - 1]
+			sp -= 1
+
+		elif ins[0] == pmac_bytecode_get:
+			stack[sp] = glob[get_str(ins[1])]
+			sp += 1
 
 		pc += 1
 
